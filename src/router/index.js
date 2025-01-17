09f3774da83d44c8dec64ab97c798ceddf3871b2
jinwei <jinweihuang2033@outlook.com>
2025-01-17 23:15:59 +0800

    change router and install gh-page package

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,10 @@ import {
 const routes = [
     {
         path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
         name: 'Home',
         component: () => import('../views/Home.vue'),
         meta: {
