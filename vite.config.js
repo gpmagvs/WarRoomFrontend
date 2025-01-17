7d8bf8efaf6219ca58dda2c43d1f722b6d6d753e
jinwei <jinweihuang2033@outlook.com>
2025-01-17 22:19:40 +0800

    change vite.config.js and add deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/WarRoomFrontend/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
@@ -14,5 +15,14 @@ export default defineConfig({
         // `
       }
     }
+  },
+  preview: {
+    port: 4173,
+    // 添加這個配置來處理 history mode 路由
+    historyApiFallback: true
+  },
+  server: {
+    // 開發模式也加上
+    historyApiFallback: true
   }
 })
