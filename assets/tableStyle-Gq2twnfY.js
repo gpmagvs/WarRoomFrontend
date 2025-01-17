d9edc0beabd6f0f52bd03219612ac26ad1f18e72
jinweihuang20 <jinweihuang20@users.noreply.github.com>
2025-01-17 15:10:26 +0000

    deploy: 1a8ff8400eb0d06cba566299c8f0934586735016

---
+++ assets/tableStyle-Gq2twnfY.js
@@ -0,0 +1,1 @@
+const o={backgroundColor:"rgb(38, 38, 38)",color:"rgb(196, 196, 196)",border:"1px solid black"},l={backgroundColor:"black",color:"rgb(196, 196, 196)",border:"1px solid black"};export{l as a,o as t};
